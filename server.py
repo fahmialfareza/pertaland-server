461838672faceb058ca270475785da8dd4f67073
Fahmi Alfareza <fahmialfareza@icloud.com>
2019-08-25 06:15:11 +0700

    New

--- server.py
+++ server.py
@@ -6,11 +6,12 @@ app = Flask("Pertaland")
 
 @app.route('/pertaland', methods=['GET'])
 def pertaland():
-    time.sleep(3)
     data = {}
     data['hotel'] = hotel.hotel(random.randint(0, 5), random.randint(0, 5), random.randint(113, 27879), random.randint(0, 7))
     data['minimarket'] = minimarket.minimarket(random.randint(0, 3), random.randint(0, 3), random.randint(5464, 19826), random.randint(3092, 21098), random.randint(0, 3))
     data['spbu'] = spbu.spbu(random.randint(954, 19865), random.randint(0, 2), random.randint(0, 5), random.randint(57, 998))
+
+    time.sleep(1)
 
     return data
 
